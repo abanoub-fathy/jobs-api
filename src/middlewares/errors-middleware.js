02bd6e68a8d117e3ebd17962ad08b58b0bf16705
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-04-01 18:41:03 +0200

    handle Mongoose Cast, validation and Duplicate Errors

--- src/middlewares/errors-middleware.js
+++ src/middlewares/errors-middleware.js
@@ -1,5 +1,30 @@
 const errorMiddleware = (err, req, res, next) => {
-  return res.status(err.status || 400).send({ error: err.message });
+  let customError = {
+    status: err.status || 500,
+    msg: err.message || "Something went wrong. Try again later",
+  };
+
+  // Dupllicate Error
+  if (err.code && err.code === 11000) {
+    customError.status = 400;
+    customError.msg = `This ${Object.keys(err.keyValue)} is already taken`;
+  }
+
+  // Validation Errors
+  if (err.name === "ValidationError") {
+    customError.status = 400;
+    customError.msg = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(", ");
+  }
+
+  // Cast Error
+  if (err.name === "CastError") {
+    customError.status = 404;
+    customError.msg = `No Item found with id ${err.value}`;
+  }
+
+  return res.status(customError.status).send({ error: customError.msg });
 };
 
 module.exports = errorMiddleware;

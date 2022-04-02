729ccc394810249d37a4058dab4513c10a121ea5
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-04-02 17:55:38 +0200

    Provide security to the app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const connectToDB = require("./db/db");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
 
 // configure express
 const app = express();
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+app.use(xss());
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 
 // routes
 app.use("/api/v1/users", require("./routes/user"));
@@ -32,3 +48,10 @@ const start = async () => {
 };
 
 start();
+
+/*
+ ** - cors
+ ** - helmet
+ ** - xss-clean
+ ** - express-rate-limit
+ */
